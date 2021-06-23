9e6608be4e9b493f361b08ed3b950bc082482a5b
unknown <Administrator@B-219-HP1.ulgam-int.tm>
2021-06-23 15:15:31 +0500

    header, footer, and login pages are done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="panel panel-default bg-dark text-light">
+    <div class="panel-body">
+        Panel Content
+    </div>
+    <div class="panel-footer">
+        Panel Footer
+    </div>
+</div>
